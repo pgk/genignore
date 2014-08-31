cf0f40c5aeb1d6fef5c3b2aaa78af73542c74cf0
Panos Kountanis <panosktn@gmail.com>
2014-08-31 12:23:40 +0300

    release on pypi

--- setup.py
+++ setup.py
@@ -2,8 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name = "genignore",
-    version = "0.1",
+    version = "0.2.1",
+    url="https://pypi.python.org/pypi/genignore",
     packages = find_packages(),
+    author="Panos Kountanis",
+    author_email="panosktn@gmail.com",
+    short_description="Generate gitignore files based on templates provided by github",
+    long_description="""\
+=========
+genignore
+=========
+
+Generate gitignore files based on templates provided by github
+
+    """,
     entry_points = {
         'console_scripts': [
             'genignore = genignore.genignore:main_func'
