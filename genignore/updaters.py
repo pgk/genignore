2a892f479747ecee54228b153eee038815c557a4
Panos Kountanis <panosktn@gmail.com>
2022-03-28 21:09:44 +0300

    update gitignore

--- genignore/updaters.py
+++ genignore/updaters.py
@@ -10,11 +10,13 @@ MASTER_ARCHIVE = "https://github.com/github/gitignore/archive/master.zip"
 
 def github(archive_url, latest_file):
     response = requests.get(archive_url, stream=True)
+    print(response)
     total_length = response.headers.get('content-length')
 
     yield total_length
 
     with open(latest_file, "wb") as f:
+        print(total_length)
         if total_length:
             downloaded = 0
             for chunk in response.iter_content(1024):
